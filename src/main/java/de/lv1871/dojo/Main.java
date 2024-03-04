09a713f3978ea5e85d961919eae6c4d796833a32
Michael Kraus <michael.kraus@lv1871.de>
2024-03-04 12:22:48 +0100

    Erster!

---
+++ src/main/java/de/lv1871/dojo/Main.java
@@ -0,0 +1,16 @@
+package de.lv1871.dojo;
+
+// In dieser Klasse soll nichts geändert werden.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public String myMethod(boolean b) {
+        return "my method!";
+    }
+
+    public static String myStaticMethod(boolean b) {
+        return "my static method!";
+    }
+}
---
+++ src/test/java/de/lv1871/dojo/EllFauhMock.java
@@ -0,0 +1,7 @@
+package de.lv1871.dojo;
+
+// In dieser Klasse soll implementiert werden. Die API darf auch (muss aber nicht) geändert werden.
+
+public class EllFauhMock {
+    // TODO implement me
+}
